40d64c9123ed563227e990d544ef594a29fcca70
Living Jiang <694771790@qq.com>
2017-02-07 11:04:21 +0800

    commit all

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,8 @@ var app = getApp(),
     url = app.globalData.url,
     booksData= app.globalData.booksData,
     navText=app.globalData.navText,
-    booksCache = {};
+    booksCache = {},
+    historyNum=-1;
 Page({
   data: {
     books:null,
@@ -15,7 +16,10 @@ Page({
     isListShow:true,
     isLoading:true,
     isSearch:false,
-    searchVal:''
+    searchVal:'',
+    eventHistory:[],
+    backDisable:true,
+    forwordDisable:true
   },
   //事件处理函数 数据请求
   getBooks: function(url,data,cb) {
@@ -30,7 +34,10 @@ Page({
   }
     });
   },
+  //导航点击获取list
   TapToGetbooks:function(e,k) {
+    // 记录事件
+    this.saveEvent("TapToGetbooks",[e,k]);
     //让list展示
     this.setData({
       isListShow:true,
@@ -75,10 +82,11 @@ Page({
   },
   //获取具体的信息
   getDetail:function(e) {
+    // 记录事件
+    this.saveEvent("getDetail",[e]);
     var that = this;
     this.setData({
       isListShow:false,
-      isLoading:true,
       detail:{}
     });
     var url = e.currentTarget.id;
@@ -89,8 +97,7 @@ Page({
   },
   success: function(res) {
     that.setData({
-      detail:res.data,
-      isLoading:false
+      detail:res.data
     });
   }
     });
@@ -99,6 +106,7 @@ Page({
     this.TapToGetbooks();
     
   },
+  // 分享
   onShareAppMessage: function () {
     return {
       title: '自定义分享标题',
@@ -131,12 +139,14 @@ Page({
     });
       }
   },
+  // 显示搜索框
   onPullDownRefresh:function() {
     this.setData({
       isSearch:true
     });
     wx.stopPullDownRefresh
   },
+  //搜索功能
   search:function(e) {
     var val = this.data.searchVal;
     var data = this.data.booksData;
@@ -164,6 +174,68 @@ Page({
       this.setData({
       isSearch:false
     });
+    }
+  },
+  //前进后退功能
+  // 记录事件
+  saveEvent:function(name,arr) {
+    var e = this.data.eventHistory;
+    var flag = false;
+    for(var i = 0; i < e.length; i++) {
+      if(e[i].name==name&&arr[0]==e[i].params[0]) {
+        flag = true;
+      }
+    }
+    if(flag) {
+      return;
+    }
+    e.length=historyNum+1;
+    e.push({name:name,params:arr});
+    historyNum++;
+    this.watch();
+    this.setData({
+      eventHistory:e
+    });
+  },
+  go:function() {
+    var event = this.data.eventHistory;
+    var i = event.length;
+    if(historyNum<i-1) {
+      historyNum++;
+      var arr = event[historyNum].params;
+      this[event[historyNum].name](arr[0],arr[1]);
+      this.watch();
+    }
+  },
+  back:function() {
+    if(historyNum>0) {
+      historyNum--;
+      var event = this.data.eventHistory;
+      var arr = event[historyNum].params;
+      this[event[historyNum].name](arr[0],arr[1]);
+      this.watch();
+    }
+  },
+  // 监听historyNum和eventHistory的变化改变按钮状态
+  watch:function() {
+    if(historyNum<=0) {
+      this.setData({
+        backDisable:true
+      });
+    }else{
+      this.setData({
+        backDisable:false
+      });
+    }
+    var i = this.data.eventHistory.length;
+    if(historyNum>=i-1) {
+      this.setData({
+        forwordDisable:true
+      });
+    }else{
+      this.setData({
+        forwordDisable:false
+      });
     }
   }
 })
